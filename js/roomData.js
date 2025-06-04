364e2d761bbb44b22690d35b36f6f7b359c00000
PhileasFogg3 <philip.beswick@hotmail.com>
2025-06-04 21:11:07 +0100

    oops I did a silly

--- js/roomData.js
+++ js/roomData.js
@@ -36,6 +36,8 @@ const rooms = {
       {
         icon: 'resources/images/icon/360 project/arrow.png',
         iconRotation: '-90 0 0',
+        iconHeight: '100',
+        iconWidth: '100',
         position: [360.32, -306.50, 160.57],
         targetRoom: 'room1',
         info: 'Get back in the kitchen!'
@@ -43,6 +45,8 @@ const rooms = {
       {
         icon: 'resources/images/icon/360 project/arrow.png',
         iconRotation: '90 0 0',
+        iconHeight: '100',
+        iconWidth: '100',
         position: [-388.99, -229.34, -213.92],
         targetRoom: 'room3',
         info: 'Go on then... check out the front door.'
@@ -59,6 +63,8 @@ const rooms = {
       {
         icon: 'resources/images/icon/360 project/arrow.png',
         iconRotation: '-90 0 0',
+        iconHeight: '100',
+        iconWidth: '100',
         position: [421.15, -228.36, -141.25],
         targetRoom: 'room2',
         info: 'Get yourself back in the hallway.'
