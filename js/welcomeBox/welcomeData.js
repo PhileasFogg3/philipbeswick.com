74cfd9d021f68f729310e220eac56d3779b4ca28
PhileasFogg3 <philip.beswick@hotmail.com>
2025-06-13 00:21:41 +0100

    Made it workable on desktop

--- js/welcomeBox/welcomeData.js
+++ js/welcomeBox/welcomeData.js
@@ -2,8 +2,8 @@ const welcomeInfo = {
     backgroundImg: [
         {
             src: 'https://mediaim.expedia.com/destination/3/03bbce489ed2090b0433dc650dbdbc44.jpg',
-            height: '50vw',
-            width: '50vw',
+            height: '30vw',
+            width: '30vw',
         }
     ],
     hero: [
