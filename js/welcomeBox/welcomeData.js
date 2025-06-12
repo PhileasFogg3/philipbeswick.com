83b8ce22ebd4f131920ed27ae8515ce586f4f60e
PhileasFogg3 <philip.beswick@hotmail.com>
2025-06-13 00:14:23 +0100

    Improved mobile accessibility

--- js/welcomeBox/welcomeData.js
+++ js/welcomeBox/welcomeData.js
@@ -2,13 +2,13 @@ const welcomeInfo = {
     backgroundImg: [
         {
             src: 'https://mediaim.expedia.com/destination/3/03bbce489ed2090b0433dc650dbdbc44.jpg',
-            height: '700px',
-            width: '1000px',
+            height: '50vw',
+            width: '50vw',
         }
     ],
     hero: [
         {
-            text: 'Welcome to my house!',
+            text: 'Welcome to my house',
             font: '',
             color: 'black',
             size: '60',
